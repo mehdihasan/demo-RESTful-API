1613ea28a9dcf049507b1aa70082345b5a9ae1f4
AAPBD <pms.aapbd@gmail.com>
2016-03-01 12:44:43 +0600

    learned how the URL and the GET, PUT, POST methofds works... now need to have much more experiment on body and header

--- lornajane/index.php
+++ lornajane/index.php
@@ -1,4 +1,7 @@
 <?php
+/*
+http://www.lornajane.net/posts/2012/building-a-restful-php-server-understanding-the-request
+*/
 $actual_link = 'http://'.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'];
 echo $actual_link."<br>\n";
 
